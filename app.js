0bd4aa889e360743f9e78ff145085ba49a42b4f2
ashmuash <ashmuash@gmail.com>
2020-08-28 11:23:30 +0530

    commited

---
+++ app.js
@@ -0,0 +1,112 @@
+const express = require("express"),
+     mongoose = require("mongoose"),
+     passport = require("passport"),
+     bodyParser = require("body-parser"),
+     User       = require("./models/user"),
+     LocalStrategy = require("passport-local"),
+     passportLocalMongoose = require("passport-local-mongoose"),
+     session = require("express-session"),
+     methodOverRide = require("method-override");
+
+const dburi = process.env.MONGO || "mongodb://localhost/auth_app";
+const sessionSecret = process.env.SESSION_SECRET || "believe in yourself";
+
+mongoose.connect(dburi, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: "secret",
+    resave: false,
+   
+    saveUninitialized: false,
+    cookie:{
+        httpOnly: false,
+
+    }
+    
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverRide('_method'));
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//================================================
+// ROUTES
+//================================================
+
+app.get("/", (req,res)=>{
+    res.render("home");
+})
+
+app.get("/secret", isLoggedIn, (req,res)=>{
+    res.render("secret");
+})
+
+// Auth Routes
+//show sign up form
+app.get("/register",(req,res)=>{
+    res.render("register");
+})
+//handling user sign up
+app.post("/register", (req,res)=>{
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}),req.body.password,(err,user)=>{
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res,()=>{
+            res.redirect("/secret");
+        })
+    });
+    
+});
+
+//LOGIN ROUTES
+//render login form
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local",{
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}),(req,res)=>{
+});
+
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.clearCookie("connect.sid");
+    
+    res.redirect('/');
+});
+
+  
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
+ 
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log("Running");
+});
