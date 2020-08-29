cae07585de4021b8165a0a0829091df5b7741d44
ashmuash <ashmuash@gmail.com>
2020-08-29 19:06:14 +0530

    final

--- admin.js
+++ admin.js
@@ -1,104 +1,106 @@
-const express = require("express");
-const User = require("./models/user");
-const user = require("./models/user");
+const express = require('express');
+const User = require('./models/user');
+const user = require('./models/user');
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+	// run for any & all requests
 
-router.use(function(req, res, next) { // run for any & all requests
-    
-    next(); // ..to the next routes from here..
-  });
+	next(); // ..to the next routes from here..
+});
 
-const uname = "admin";
-const pword = "admin";
+const uname = 'admin';
+const pword = 'admin';
 
-const protection = (req,res,next)=>{
-    if (req.session.username){   
-        next();
-    }else{
-        res.redirect("/admin");
-    }
-}
+const protection = (req, res, next) => {
+	if (req.session.username) {
+		next();
+	} else {
+		res.redirect('/admin');
+	}
+};
 
+const isAuth = (req, res, next) => {
+	if (req.session.username) {
+		res.redirect('/admindashboard');
+	} else {
+		next();
+	}
+};
 
+router.get('/admin', isAuth, (req, res) => {
+	res.render('admin', { errors: null, username: null });
+});
 
-router.get("/admin",(req,res)=>{
-    res.render("admin");
-})
+router.post('/admin', (req, res) => {
+	const { username, password } = req.body;
 
-router.post("/admin",(req,res)=>{
-    const {username,password}=req.body;
-    
+	if (username === uname && password === pword) {
+		req.session.username = username;
+		res.redirect('/admindashboard');
+	} else {
+		// res.send(500,'showAlert')
+		res.render('admin', {
+			errors: 'invalid username or password',
+			username
+		});
+	}
+});
+router.get('/admindashboard', protection, (req, res) => {
+	User.find({}).lean().exec((err, data) => {
+		res.render('admindashboard', { user: data });
+	});
+});
 
-    if(username === uname && password === pword){
-        req.session.username = username;
-        res.redirect("/admindashboard");
-       
-    }else{ 
-        // window.alert("Incorrect Username or Password");
-        res.redirect("/admin");
-       
-       
-    }
-})
-router.get("/admindashboard", protection,(req,res)=>{
-   User.find({}).lean().exec((err,data)=>{
-      res.render("admindashboard",{user:data});
-        
-    })
-})
+router.post('/useredit', (req, res) => {
+	const email = req.body.email;
+	User.updateOne({ email: email });
+});
 
-router.post("/useredit", (req,res)=>{
-    const email = req.body.email;
-    User.updateOne({email:email},)
-})
+router.get('/adminedit', (req, res) => {
+	let username = req.body.username;
 
-router.get("/adminedit",(req,res)=>{
+	res.render('adminedit');
+});
 
-    let username = req.body.username;
+router.post('/adminedit', (req, res) => {
+	let email = req.body.email;
+	User.findOne({ email: email }).lean().exec((err, data) => {
+		if (data) {
+			res.render('adminedit', { data: data });
+		} else {
+			res.redirect('/admindashboard');
+		}
+	});
+});
 
-    res.render("adminedit");
-})
+router.post('/editsave', (req, res) => {
+	const { username, email } = req.body;
+	user.updateOne({ email: email }, { $set: { username: username, email: email } }, (err) => {
+		if (err) {
+			throw err;
+		} else {
+			res.redirect('admindashboard');
+		}
+	});
+});
 
-router.post("/adminedit", (req,res)=>{
-    let email = req.body.email;
-    User.findOne({email:email}).lean().exec((err,data)=>{
-        if(data){
-            res.render("adminedit",{data:data});
-          
-        }else{
-            res.redirect("/admindashboard");
-        }
-    })
-} )
-
-router.post("/editsave", (req,res)=>{
-    const {username,email}=req.body;
-    user.updateOne({email:email},{$set:{username:username,email:email}},(err)=>{
-        if (err){
-            throw err;
-        }else{
-            res.redirect("\admindashboard");
-        }
-    })
-})
-
-router.post("/deleteuser",(req,res)=>{
-    const email = req.body.email;
-    user.deleteOne({email:email},(err)=>{
-        if (err) {
-            throw err;
-        }else{
-            res.redirect("/admindashboard");
-        }
-    })
-})
+router.post('/deleteuser', (req, res) => {
+	const email = req.body.email;
+	user.deleteOne({ email: email }, (err) => {
+		if (err) {
+			throw err;
+		} else {
+			res.redirect('/admindashboard');
+		}
+	});
+});
 
 router.get('/adminlogout', function(req, res) {
-    req.session.destroy();
-    res.clearCookie("connect.sid");
-    res.redirect('/admin');
+	req.session.destroy();
+	res.clearCookie('connect.sid');
+	res.redirect('/admin');
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,121 +1,119 @@
-const express = require("express"),
-     mongoose = require("mongoose"),
-     passport = require("passport"),
-     bodyParser = require("body-parser"),
-     User       = require("./models/user"),
-     LocalStrategy = require("passport-local"),
-     passportLocalMongoose = require("passport-local-mongoose"),
-     session = require("express-session"),
-     methodOverRide = require("method-override"),
-     admin = require("./admin")
-    
+const express = require('express'),
+	mongoose = require('mongoose'),
+	passport = require('passport'),
+	bodyParser = require('body-parser'),
+	User = require('./models/user'),
+	LocalStrategy = require('passport-local'),
+	passportLocalMongoose = require('passport-local-mongoose'),
+	session = require('express-session'),
+	methodOverRide = require('method-override'),
+	admin = require('./admin');
 
-
-
-
-const dburi = process.env.MONGO || "mongodb://localhost/auth_app";
-const sessionSecret = process.env.SESSION_SECRET || "believe in yourself";
+const dburi = process.env.MONGO || 'mongodb://localhost/auth_app';
+const sessionSecret = process.env.SESSION_SECRET || 'believe in yourself';
 
 mongoose.connect(dburi, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+	useNewUrlParser: true,
+	useUnifiedTopology: true
 });
 
-
 var app = express();
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(session({
-    secret: "secret",
-    resave: false,
-   
-    saveUninitialized: false,
-    cookie:{
-        httpOnly: false,
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+	session({
+		secret: 'secret',
+		resave: false,
 
-    }
-    
-}));
+		saveUninitialized: false,
+		cookie: {
+			httpOnly: false
+		}
+	})
+);
 
-app.use("/",admin);
+app.use((req, res, next) => {
+	res.set('Cache-Control', 'no-store');
+	next();
+});
+app.use('/', admin);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverRide('_method'));
-
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+const isAuth = (req, res, next) => {
+    if(req.isAuthenticated()){
+		res.redirect('/secret')
+	}else{
+		next()
+	}
+
+};
+
 //================================================
 // ROUTES
 //================================================
 
-app.get("/", (req,res)=>{
-    res.render("home");
-})
+app.get('/', (req, res) => {
+	res.render('home');
+});
 
-app.get("/secret", isLoggedIn, (req,res)=>{
-    res.render("secret");
-})
+app.get('/secret', (req, res) => {
+	res.render('secret');
+});
 
 // Auth Routes
 //show sign up form
-app.get("/register",(req,res)=>{
-    res.render("register");
-})
+app.get('/register', (req, res) => {
+	res.render('register');
+});
 //handling user sign up
-app.post("/register", (req,res)=>{
-     
-     username = req.body.username;
-     password = req.body.password;
-     email = req.body.email;
-    
-    User.register(new User({username: req.body.username, email:req.body.email}),req.body.password,(err,user)=>{
-        if(err){ 
-            console.log(err);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res,()=>{
-            res.redirect("/login");
-        })
-    });
-    
+app.post('/register', (req, res) => {
+	username = req.body.username;
+	password = req.body.password;
+	email = req.body.email;
+
+	User.register(new User({ username: req.body.username, email: req.body.email }), req.body.password, (err, user) => {
+		if (err) {
+			console.log(err);
+			return res.render('register');
+		}
+		passport.authenticate('local')(req, res, () => {
+			res.redirect('/login');
+		});
+	});
 });
 
 //LOGIN ROUTES
 //render login form
-app.get("/login",(req,res)=>{
-    res.render("login");
+app.get('/login',isAuth, (req, res) => {
+	res.render('login');
 });
 //login logic
 //middleware
-app.post("/login", passport.authenticate("local",{
-    successRedirect: "/secret",
-    failureRedirect: "/login"
-}),(req,res)=>{
-});
-
+app.post(
+	'/login',
+	passport.authenticate('local', {
+		successRedirect: '/secret',
+		failureRedirect: '/login'
+	}),
+	(req, res) => {}
+);
 
 app.get('/logout', function(req, res) {
-    req.logout();
-    res.clearCookie("connect.sid");
-    
-    res.redirect('/');
+	req.logout();
+	res.clearCookie('connect.sid');
+
+	res.redirect('/');
 });
 
-  
-
-function isLoggedIn(req,res,next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
 
 
- 
-app.listen(process.env.PORT || 8080, ()=>{
-    console.log("Running");
+app.listen(process.env.PORT || 8080, () => {
+	console.log('Running');
 });
